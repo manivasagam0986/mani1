e3ada2b1ffef59e337df5f8dc1dfc34bd3036e57
manivasagam0986 <manivasagam33043@gmail.com>
2024-05-07 16:52:08 +0530

    my task

---
+++ org/wipro/Employee.java
@@ -0,0 +1,15 @@
+package org.wipro;
+
+public class Employee {
+
+	private void bmw() {
+    System.out.println("bmw");
+	}
+	private void audi() {
+    System.out.println("audi");
+	}
+	private void susu() {
+    System.out.println("susu");
+	}
+	
+}
